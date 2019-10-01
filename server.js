fadab164d137bf8c1372a33db0f9f8293b7e210f
Xavier <provost.pascal@gmail.com>
2019-10-01 02:08:16 -0400

    Added css files to shoot, some documentation added

--- server.js
+++ server.js
@@ -7,10 +7,16 @@ console.log("HTTP server initialization...");
 var app = express();
 
 
+
+/**
+ * This will generate an empty page with a script tag, which contains an alert with the provided message
+ * @param msg {string} message to show in the alert
+ */
 app.get('/exploits/alert/:msg', function(req,res){
 	'use strict';
-    let alertMsg;
-	
+
+	let alertMsg;
+	// This is just additional protection, who knows what could happen with an empty string
 	if(req.params.msg == "" || !req.params.msg){
 		alertMsg = "Default message";
 	}
@@ -23,11 +29,17 @@ app.get('/exploits/alert/:msg', function(req,res){
 	res.send(page);
 });
 
+
+
+/**
+ * This will generate a page with a single img tag, which source is provided
+ * @param url {string} URL of the image, please use URL Encoded strings
+ */
 app.get('/exploits/image/:url', function(req,res){
 	'use strict';
     
 	let imageUrl;
-	
+	// This is just additional protection, who knows what could happen with an empty string
 	if(req.params.url == "" || !req.params.url){
 		imageUrl = "https%3A%2F%2Fi.ytimg.com%2Fvi%2Fej6gpF4TDnA%2Fmaxresdefault.jpg";
 	}
@@ -42,10 +54,29 @@ app.get('/exploits/image/:url', function(req,res){
 
 
 
+/**
+ * This will serve a single css file, which is specified in the query. Will default to blackout.css
+ * @param file {string} name of the file
+ */
+app.get('/exploits/css/:file', function(req,res){
+	'use strict';
+
+	if(req.params.file == "blackout"){
+		res.sendFile("./client/css/blackout.css");
+	}
+	else{
+		res.sendFile("./client/css/blackout.css");
+	}
+});
+
+
+
+// Serving a single example page if no specific URL present
 app.use(express.static('./client'));
 
+//Had to use process.env.port for Heroku to run it in whatever port it needs, will default to 21134 if none provided
 app.listen(process.env.PORT || 21134, function () {
     "use strict";
     
-	console.log("Simple HTTP Server running");
+	console.log("Simple HTTP Server running on port" + process.env.port || 21134);
 });
