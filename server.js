f31d7f94f3f0e12214712e869306689c94dbb9e8
Xavier <propasc@gmail.com>
2019-09-30 22:45:25 -0400

    changed directory arbo

--- server.js
+++ server.js
@@ -7,17 +7,17 @@ console.log("HTTP server initialization...");
 var app = express();
 
 
-app.get('/exploits/:type', function(req,res){
+/*app.get('/exploits/:type', function(req,res){
 	'use strict';
     
     if(req.params.type == "image"){
 		
 	}
-});
+});*/
 
 
 
-app.use(express.static('../client'));
+app.use(express.static('./client'));
 
 app.listen(process.env.PORT || 21134, function () {
     "use strict";
