87cf8d8ec1c9d3c91cf93faf11b50e1218967f5f
Xavier <propasc@gmail.com>
2019-09-30 22:37:18 -0400

    Corrected server launch and added engines to package.json

--- server/server.js
+++ server/server.js
@@ -6,6 +6,17 @@ const express = require('express');
 console.log("HTTP server initialization...");
 var app = express();
 
+
+app.get('/exploits/:type', function(req,res){
+	'use strict';
+    
+    if(req.params.type == "image"){
+		
+	}
+});
+
+
+
 app.use(express.static('../client'));
 
 app.listen(process.env.PORT || 21134, function () {
